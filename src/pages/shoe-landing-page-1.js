77e8f1e288cc283367a6e5b61cd53a6a57827a43
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2021-05-29 15:55:00 -0600

    Removing shoe landing page for now

--- src/pages/shoe-landing-page-1.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import ShoeLandingPage1Content from '../components/shoelandingpage1'
-
-const ShoeLandingPage1 = () => <ShoeLandingPage1Content />
-
-export default ShoeLandingPage1
